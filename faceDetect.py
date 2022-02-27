8abd8e65cff43cb43195de59e4650e379bd2cd59
zaid <zaidpirwani786@gmail.com>
2022-02-27 04:35:58 +0000

    image cannot read and face ROI issue, need to add padding to face

--- faceDetect.py
+++ faceDetect.py
@@ -19,6 +19,10 @@ if __name__ == "__main__":
         print('FILENAME WITH PATH:' + imageFileName)
         # Read the input image
         img = cv2.imread(imageFileName)
+        if img is None:
+            csvwriter.writerow([imageFileName,'CANNOT READ IMAGE'])
+            print(row[0], ": CANNOT READ IMAGE")
+            continue
         # Convert into grayscale
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         # Detect faces
@@ -27,16 +31,20 @@ if __name__ == "__main__":
         padding=0.2 #percentage padding
         if len(faces)==0:
             csvwriter.writerow([imageFileName,'NO FACE FOUND'])
-            print("NO FACES FOUND")
+            print(row[0], ": NO FACES FOUND")
         else:
             rowToWrite = []
             rowToWrite.append(imageFileName)
             a=0
             for (x, y, w, h) in faces:
-                y1 = 0 if int(y-(h*padding))  <0 else int(y-(h*padding))
-                y2 = h if int(y+h+(h*padding))>h else int(y+h+(h*padding))
-                x1 = 0 if int(x-(w*padding))  <0 else int(x-(w*padding))
-                x2 = w if int(x+w+(w*padding))>w else int(x+w+(w*padding))
+                #y1 = 0 if int(y-(h*padding))  <0 else int(y-(h*padding))
+                #y2 = h if int(y+h+(h*padding))>h else int(y+h+(h*padding))
+                #x1 = 0 if int(x-(w*padding))  <0 else int(x-(w*padding))
+                #x2 = w if int(x+w+(w*padding))>w else int(x+w+(w*padding))
+                y1 = y
+                y2 = y+h
+                x1 = x
+                x2 = x+w
                 print (y1, ", ", y2, ", ", x1, ", ", x2)
                 roi = img[y1:y2,x1:x2]
                 if a>0:
